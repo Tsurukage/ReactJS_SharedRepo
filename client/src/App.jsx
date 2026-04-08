a2d9774624a66390760c57d2a7730449a458732d
Tsurukage <chui.lim3991@gmail.com>
2026-04-08 21:43:44 +0800

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-cyan-950">
+      <div className="bg-cyan-900 flex justify-center p-2">
+        <p className="text-xl font-semibold text-white">Test Test</p>
+      </div>
+      <div className="flex justify-center py-1 px-4">
+        <button className="bg-blue-500 px-4 py-2 rounded">Click Me 0</button>
+        <button className="bg-blue-500 px-4 py-2 rounded">Click Me 1</button>
+        <button className="bg-blue-500 px-4 py-2 rounded">Click Me 2</button>
+        <button className="bg-blue-500 px-4 py-2 rounded">Click Me 3</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
